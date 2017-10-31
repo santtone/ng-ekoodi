16f7a9232d0aeb5fcfdb4ffaed29adbca4364526
santtone <sami.anttonen@trimble.com>
2017-10-31 11:15:44 +0200

    Added Angular Material styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
-import { UserListComponent } from './user/user-list/user-list.component';
-import { UserInfoComponent } from './user/user-info/user-info.component';
+import {UserListComponent} from './user/user-list/user-list.component';
+import {UserInfoComponent} from './user/user-info/user-info.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatIconModule, MatInputModule, MatLineModule, MatListModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -13,9 +15,17 @@ import { UserInfoComponent } from './user/user-info/user-info.component';
     UserInfoComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatListModule,
+    MatLineModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
